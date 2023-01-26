0ab6da285e115e1282c9c8ec1f582883ea310928
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 21:23:46 +0600

    variables

---
+++ variable.js
@@ -0,0 +1,21 @@
+var name = 'Sabbir';
+var sunglassPrice = 120;  
+
+// number type variable 
+var penPrice = 5;
+
+// string type variable 
+var speical = 'Anu';
+
+// boolean type variable 
+var isSingle = true;
+
+// two types of number 
+// Integer - 12
+// Float Number - 5.2
+
+// Math Oparations : + , - , * , / ,
+// Shorthand ++ , -- , -= , += ,
+
+// tofixed 
+// remainder : vagses % 
