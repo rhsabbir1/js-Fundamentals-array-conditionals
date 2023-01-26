794ec474aa00f8d5f77058902a3f876cd2c0c32e
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 21:41:52 +0600

    array declear

---
+++ array.js
@@ -0,0 +1,13 @@
+// array declearation 
+var age = [12 , 15, 21, 52, 25];
+var friendsName = ['Atik' ,'Nur' , 'Mahin' ,'Shadin'];
+var vowels = [ 'a' , 'e ', 'i' ,'o' ,'u'];
+
+// console.log(age)
+// console.log(friendsName)
+// console.log(vowels)
+
+// Array Elements 
+console.log(age.length)
+console.log(friendsName.length)
+console.log(vowels.length)
