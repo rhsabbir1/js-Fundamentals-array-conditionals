3881539992b12b4bbadd480503ce713f58ea2339
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 22:38:04 +0600

    add and remove elements of array

---
+++ push-pop.js
@@ -0,0 +1,22 @@
+var numbers = [11 ,12 ,55 ,14 ,32, 25];
+numbers.push(22);
+console.log(numbers)
+
+var actors = ['Ertugrul' , 'Turgut' , 'Bamsi'];
+// add Element of array 
+actors.push('Abdur-Rahman')
+actors.push('Dugan')
+actors.push('Arris')
+console.log(actors)
+
+// remove elemenst of array 
+actors.pop()
+actors.pop()
+var dead =actors.pop()
+console.log(dead)
+console.log(actors)
+
+var vowel = ['a','e','i','o', 'u']
+vowel.unshift('k')
+vowel.shift()
+console.log(vuwel)
