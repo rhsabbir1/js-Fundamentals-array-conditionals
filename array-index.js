d8ad8cde488dffd265943bbc6037b511801e8caa
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-26 22:05:48 +0600

    array-index

---
+++ array-index.js
@@ -0,0 +1,16 @@
+var friendage = [15, 18, 13, 16, 14, 19];
+
+//1. get elements by index 
+var elderFridenAge = friendage[2];
+// console.log(elderFridenAge)
+
+//2. set valur by index 
+friendage[2] = 16;
+// console.log(friendage)
+friendage[3] = 21;
+// console.log(friendage)
+
+// 3.Find index of array 
+var positionIndex = friendage.indexOf(19)
+// var positionIndex = friendage.indexOf(190) (-1)
+console.log(positionIndex)
