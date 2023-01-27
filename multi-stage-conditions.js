d5bbaa86109cc3d3673fe492266a290cd40eb3ae
rhsabbir1 <rhsabbir090@gmai.com>
2023-01-27 14:35:00 +0600

    multiple condition and nested condition

---
+++ multi-stage-conditions.js
@@ -0,0 +1,40 @@
+var money = 46;
+var biskitPrice = 50;
+var danishPrice = 45;
+var butterBanPrice = 25;
+var teaPrice = 5;
+
+if(money > biskitPrice){
+    console.log("Biskit Den")
+}
+else if(danishPrice < money){
+    console.log("Danish den")
+}
+else if(butterBanPrice < money){
+    console.log('Butter Ban Dan')
+}
+else{
+    console.log('Tea is good for health')
+}
+
+// nasted condition 
+
+if(condition){
+
+    if(condition){
+
+        if(condition){
+
+        }
+        else{
+
+        }
+    }
+    else{
+
+    }
+}
+
+else{
+
+}
